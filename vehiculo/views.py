8f98c2df30bc578858f243533fe79cff9ff4ce18
ivansp <ivan.sp100@gmail.com>
2023-07-19 13:40:14 -0400

    se comprueba que urls funciona

--- vehiculo/views.py
+++ vehiculo/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
+
+def index(request):
+    return HttpResponse("<h1> bienvenidos</h1>")
 # Create your views here.
